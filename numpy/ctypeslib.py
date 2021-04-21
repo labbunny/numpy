1d5f4f74fc97ac94374eb142163a1d3bcb05353c
Luka Mihalič <76424796+labbunny@users.noreply.github.com>
2021-04-21 12:10:26 +0200

    MAINT: Chain exceptions in ctypeslib.py
    This solution is related to issue #15986

--- numpy/ctypeslib.py
+++ numpy/ctypeslib.py
@@ -147,9 +147,9 @@ else:
             if os.path.exists(libpath):
                 try:
                     return ctypes.cdll[libpath]
-                except OSError:
+                except OSError as e:
                     ## defective lib file
-                    raise
+                    raise ValueError("defective lib file") as e
         ## if no successful return in the libname_ext loop:
         raise OSError("no file with expected extension")
 
